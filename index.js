f26ae8b4abd726980c7a2eaaa676d5c677283dc4
Jacob Rathbun <jrathofbun@gmail.com>
2023-04-25 20:30:11 -0400

    Complete Lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(drivers, driverName) {
+    return drivers.filter(driver=> driver.toLowerCase() === driverName.toLowerCase());
+}
+
+function fuzzyMatch(drivers, letter) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(letter.toLowerCase()));
+}
+
+function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+}
